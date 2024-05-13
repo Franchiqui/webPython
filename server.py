07e8aa5db06d700c288359edace4fe7360ede90e
FranchiquiJ <167950323+FranchiquiJ@users.noreply.github.com>
2024-05-13 18:44:33 +0200

    g 	deleted:    Dockerfile 	deleted:    app/__init__.py 	modified:   server.py 	deleted:    traductor.py

--- server.py
+++ server.py
@@ -1,47 +1,56 @@
-import socket
+import socket 
 
-def traductor(request_data):(str)
+host , port = '127.0.0.1' , 8888
 
-host, port = '127.0.0.1', 8888
 serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-serversocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-serversocket.bind((host, port))
+serversocket.setsockopt(socket.SOL_SOCKET , socket.SO_REUSEADDR , 1)
+serversocket.bind((host , port))
 serversocket.listen(1)
-print('Servidor en el puerto', port)
-
-def process_request(request):
-    parts = request.split()
-    method = parts[0]
-    path = parts[1]
-
-    if path == '/':
-        myfile = 'index.html'
-    else:
-        myfile = path.lstrip('/')
-
-    try:
-        if method == 'GET' and path == '/':
-            header = 'HTTP/1.1 200 OK\n\n'
-            response = '<html><body>Hello, world!</body></html>'.encode('utf-8')
-        elif method == 'POST' and path == '/traductor':
-            request_data = request.split('\r\n\r\n')[1]
-            translate_text, target_lang = request_data.split('&')
-            translate_text = translate_text.split('=')[1]
-            target_lang = target_lang.split('=')[1]
-            traduccion = traductor(translate_text, target_lang)
-            response = f'HTTP/1.1 200 OK\nContent-Type: text/plain\n\n{traduccion}'.encode('utf-8')
-        else:
-            response = 'HTTP/1.1 404 Not Found\nContent-Type: text/plain\n\nError 404: Recurso no encontrado'.encode('utf-8')
-
-        return response
-    except Exception as e:
-        return f'HTTP/1.1 500 Internal Server Error\nContent-Type: text/plain\n\n{str(e)}'.encode('utf-8')
+print('servidor en el puerto',port)
 
 while True:
-    connection, address = serversocket.accept()
+    connection , address = serversocket.accept()
     request = connection.recv(1024).decode('utf-8')
-    print('Cliente solicitó', request)
+    #print(request)
+    string_list = request.split(' ')
+    method = string_list[0]
+    requesting_file = string_list[1]
 
-    response = process_request(request)
-    connection.send(response)
+    print('Client request',requesting_file)
+
+    myfile = requesting_file.split('?')[0]
+    myfile = myfile.lstrip('/')
+
+    if(myfile == ''):
+        myfile = 'index.html'
+
+    try:
+        file = open(myfile , 'rb')
+        response = file.read()
+        file.close()
+
+        header = 'HTTP/1.1 200 OK\n'
+
+        if(myfile.endswith('.jpg')):
+            mimetype = 'image/jpg'
+        elif(myfile.endswith('.css')):
+            mimetype = 'text/css'
+        elif(myfile.endswith('.pdf')):
+            mimetype = 'application/pdf'
+        else:
+            mimetype = 'text/html'
+
+        header += 'Content-Type: '+str(mimetype)+'\n\n'
+
+    except Exception as e:
+        print("-")
+        header = 'HTTP/1.1 404 Not Found\n\n'
+        response = '<html><body>Error 404: File not found</body></html>'.encode('utf-8')
+
+    final_response = header.encode('utf-8')
+    final_response += response
+    connection.send(final_response)
     connection.close()
+
+
+    
